dbda1d85dbb6bc86903eb925b28c85ff1ae3415d
Damileee <olasojudamilare6@gmail.com>
2025-09-28 19:50:00 +0100

    chore(invoice): implemented  action cards component for reusability

---
+++ frontend/components/InvoiceActionCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    class="btn-base flex flex-col items-start py-8 px-9 !rounded-3xl shadow-sm transition-all duration-150 cursor-pointer w-full group"
+    role="button"
+    aria-label="invoice action card"
+  >
+    <!-- Icon -->
+    <div class="mb-4">
+      <component :is="icon" width="80" height="80" />
+    </div>
+
+    <!-- Title -->
+    <h3
+      class="text-[22px] font-medium mb-2 text-[#373B47] group-hover:text-white"
+    >
+      {{ title }}
+    </h3>
+
+    <!-- Subtitle -->
+    <p class="text-sm text-[#697598] group-hover:text-[#F6F8FA]">
+      {{ subtitle }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  icon: any;
+  title: string;
+  subtitle?: string;
+}
+
+const props = defineProps<Props>();
+</script>

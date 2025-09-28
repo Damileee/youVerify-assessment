a400bb726ed48d228907b471fada6bb5f61f8fd1
Damileee <olasojudamilare6@gmail.com>
2025-09-28 19:10:00 +0100

    chore: implemented default layout for the page

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex h-screen bg-[#F5F6FA] font-sans">
+    <!-- Sidebar -->
+    <CommonSidebar :isOpen="isSidebarOpen" @close="isSidebarOpen = false" />
+
+    <!-- Main content -->
+    <div class="flex-1 flex flex-col lg:mx-10">
+      <!-- Header -->
+      <HeaderBar title="Invoice" />
+
+      <!-- Page content -->
+      <main class="p- flex-1 overflow-auto no-scrollbar">
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const isSidebarOpen = ref(false);
+</script>

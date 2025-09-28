09bf1137b5be40d8d5ab223d2ef97e7b173dfe52
Damileee <olasojudamilare6@gmail.com>
2025-09-28 20:00:37 +0100

    chore(recent invoice): completed recent invoice UI

---
+++ frontend/components/RecentInvoices.vue
@@ -0,0 +1,44 @@
+<template>
+  <ContainerInvoice
+    title="Recent Invoices"
+    buttonLabel="View All invoices"
+    @buttonClick="
+      () => {
+        /* Handle button click */
+      }
+    "
+  >
+    <div class="space-y-6">
+      <div v-for="(group, gIndex) in invoiceGroups" :key="gIndex">
+        <!-- Group Header -->
+        <p class="text-xs font-medium text-[#1F1F23] my-[10px]">
+          {{ group.date }}
+        </p>
+
+        <!-- Invoices in Group -->
+        <div>
+          <InvoiceItem
+            v-for="(invoice, iIndex) in group.invoices"
+            :key="iIndex"
+            v-bind="invoice"
+          />
+        </div>
+      </div>
+    </div>
+  </ContainerInvoice>
+</template>
+
+<script setup lang="ts">
+import type { Invoice } from "~/types/invoice";
+
+const props = defineProps<{
+  invoiceGroups: {
+    date: string;
+    invoices: Invoice[];
+  }[];
+}>();
+
+const handleViewAll = () => {
+  console.log("View all invoices clicked");
+};
+</script>

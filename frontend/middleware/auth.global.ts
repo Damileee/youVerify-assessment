4abbbdcefd02bde0d0985a7c1164a69f71a46029
Damileee <olasojudamilare6@gmail.com>
2025-09-29 12:21:38 +0100

    chore: imtegrated middleware

---
+++ frontend/middleware/auth.global.ts
@@ -0,0 +1,18 @@
+export default defineNuxtRouteMiddleware((to) => {
+  const publicRoutes = ["/auth/login", "/auth/register"];
+  const authCookie = useCookie<string | null>("firebaseToken");
+
+  // If logged in and trying to access login/register → redirect to home
+  if (authCookie.value && publicRoutes.includes(to.path)) {
+    return navigateTo("/invoice");
+  }
+
+  if (authCookie.value && to.path === "/") {
+    return navigateTo("/invoice")
+  }
+
+  // If not logged in and trying to access private routes → redirect to login
+  if (!authCookie.value && !publicRoutes.includes(to.path)) {
+    return navigateTo("/auth/login");
+  }
+});

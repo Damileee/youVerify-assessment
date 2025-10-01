f28e0a4a725f386d5b72f51288e3dc3b25722102
Damileee <olasojudamilare6@gmail.com>
2025-10-01 10:23:58 +0100

    update: added loading state to auth button

--- frontend/components/AuthForm.vue
+++ frontend/components/AuthForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col items-center justify-center min-h-screen">
+  <div class="flex flex-col mx-10 lg:items-center justify-center min-h-screen">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
       <h2
         class="mt-10 text-center text-2xl font-bold tracking-tight text-gray-900"
@@ -15,9 +15,17 @@
         <div>
           <button
             type="submit"
-            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            :disabled="loading"
+            class="flex w-full items-center justify-center gap-2 rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-xs 
+              hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600
+              disabled:opacity-50 disabled:cursor-not-allowed"
           >
-            {{ buttonText }}
+            <span>{{ buttonText }}</span>
+            <!-- Loader beside text -->
+            <div
+              v-if="loading"
+              class="w-4 h-4 border-2 border-white border-t-transparent rounded-full animate-spin"
+            ></div>
           </button>
         </div>
       </form>
@@ -40,12 +48,15 @@ const props = defineProps<{
   title: string;
   buttonText: string;
   onSubmit: () => void;
+  loading?: boolean;
   footerText?: string;
   footerAction?: string;
   footerLink?: string;
 }>();
 
 const handleSubmit = () => {
-  props.onSubmit();
+  if (!props.loading) {
+    props.onSubmit();
+  }
 };
 </script>
